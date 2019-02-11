7813bd932141a5fd0ce522d9f42e05d27433041a
Nicolas Crocfer <ncrocfer@gmail.com>
2019-02-11 16:55:09 +0100

    Do not fetch all users during the first display (#6)
    * feat(permissions): do not fetch all users during the first display
    
    Signed-off-by: Nicolas Crocfer <nicolas.crocfer@corp.ovh.com>
    
    * fix(api): only users of a team can display its QoS
    
    Signed-off-by: Nicolas Crocfer <nicolas.crocfer@corp.ovh.com>

--- ui/app/scripts/controllers/team/permissions.js
+++ ui/app/scripts/controllers/team/permissions.js
@@ -8,38 +8,58 @@
  * Controller of the depcwebuiApp
  */
 angular.module('depcwebuiApp')
-  .controller('PermissionsCtrl', function ($routeParams, $rootScope, toastr, teamsService, usersService) {
+  .controller('PermissionsCtrl', function ($routeParams, $scope, toastr, teamsService, usersService) {
     var self = this;
 
     self.teamName = $routeParams.team;
     self.search = '';
     self.load = true;
+    self.includeOther = false;
 
     this.refresh = function() {
         self.load = true;
         self.users = [];
         self.grants = [];
 
-        usersService.getUsers().then(function(response) {
-            self.users = response.data.users;
-        });
-
         teamsService.getTeamByName(self.teamName).then(function(response) {
             self.team = response.data;
 
-            teamsService.getTeamGrants(self.team.id).then(function(response) {
-                var grants = response.data;
-                for ( var grant in grants ) {
-                    self.grants[grants[grant]['user']] = grants[grant]['role'];
-                };
+            usersService.getUsers().then(function(response) {
+                var users = response.data.users;
 
-                self.load = false;
+                teamsService.getTeamGrants(self.team.id).then(function(response) {
+                    var grants = response.data;
+                    var grantedUsers = [];
+                    for ( var grant in grants ) {
+                        grantedUsers.push(grants[grant].user);
+                        self.grants[grants[grant]['user']] = grants[grant]['role'];
+                    };
+
+                    if ( self.includeOther ) {
+                        self.users = users;
+                    } else {
+                        var users_copy = []
+                        for ( var user in users ) {
+                            if ( grantedUsers.indexOf(users[user].name) > -1 ) {
+                                users_copy.push(users[user]);
+                            }
+                        }
+                        self.users = users_copy;
+                    }
+
+                    self.load = false;
+                });
             });
         });
     };
 
     // Load the data
     self.refresh();
+    $scope.$watch(function() {
+        return self.includeOther;
+      }, function() {
+          self.refresh();
+      }, true);
 
     this.applyGrants = function() {
         self.load = true;
