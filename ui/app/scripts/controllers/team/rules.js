56c2503d54b520099a334e96bae9a562cf5ebd06
Nicolas Crocfer <nicolas.crocfer@corp.ovh.com>
2019-03-21 15:17:03 +0100

    fix(ui): bad state in rule's panel result
    Signed-off-by: Nicolas Crocfer <nicolas.crocfer@corp.ovh.com>

--- ui/app/scripts/controllers/team/rules.js
+++ ui/app/scripts/controllers/team/rules.js
@@ -215,7 +215,7 @@ angular.module('depcwebuiApp')
         // No result means no panel
         if ( self.result != null ) {
 
-            var qos = self.result.qos;
+            var qos = self.result.qos.qos;
 
             if ( qos == 'unknown' ) {
                 return panel[key];
