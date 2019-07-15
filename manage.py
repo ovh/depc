909707d7063a803c9e2f5dc99213e63018b02f0e
Anthony Olea <anthony.olea@corp.ovh.com>
2019-07-15 12:13:32 +0200

    feat(cli): remove flask-script, use click instead, rework some commands
    Signed-off-by: Anthony Olea <anthony.olea@corp.ovh.com>

--- manage.py
+++ manage.py
@@ -1,31 +1,15 @@
 import os
 
-from flask_script import Shell, Manager
-from flask_script.commands import ShowUrls
-from flask_migrate import MigrateCommand
-
 from depc import create_app
-from depc.commands.getconfig import GetConfig
-from depc.commands.key import key_manager
+from depc.commands.config import config_cli
+from depc.commands.key import key_cli
+
+env = os.getenv("DEPC_ENV", "dev")
+
+app = create_app(environment=env)
+app.cli.add_command(config_cli)
+app.cli.add_command(key_cli)
 
 
-# Create an application using the `api` context
-app = create_app(
-    environment=os.getenv('DEPC_ENV') or 'dev',
-)
-manager = Manager(app)
-
-
-@manager.shell
-def make_shell_context():
-    return dict(app=app)
-
-
-manager.add_command('shell', Shell(make_context=make_shell_context))
-manager.add_command('getconfig', GetConfig(app))
-manager.add_command('db', MigrateCommand)
-manager.add_command('urls', ShowUrls)
-manager.add_command('key', key_manager)
-
-if __name__ == '__main__':
-    manager.run()
+if __name__ == "__main__":
+    app.run(debug=(env == "dev"))
