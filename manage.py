ae1eaf527c47c5dbfd93a7f6f77082608346f51d
Anthony Olea <anthony.olea@corp.ovh.com>
2019-07-16 16:05:52 +0200

    feat(cli): add command to create user
    Signed-off-by: Anthony Olea <anthony.olea@corp.ovh.com>

--- manage.py
+++ manage.py
@@ -3,12 +3,14 @@ import os
 from depc import create_app
 from depc.commands.config import config_cli
 from depc.commands.key import key_cli
+from depc.commands.user import user_cli
 
 env = os.getenv("DEPC_ENV", "dev")
 
 app = create_app(environment=env)
 app.cli.add_command(config_cli)
 app.cli.add_command(key_cli)
+app.cli.add_command(user_cli)
 
 
 if __name__ == "__main__":
