2a4575ddd7c2fc897c2dd5759d48575943fdedbc
Anthony Olea <anthony.olea@corp.ovh.com>
2019-10-25 14:41:06 +0200

    feat(config, scheduler): add exclusion list of nodes from the label average value
    Signed-off-by: Anthony Olea <anthony.olea@corp.ovh.com>

--- scheduler/dags/operators/before_subdag_operator.py
+++ scheduler/dags/operators/before_subdag_operator.py
@@ -2,6 +2,7 @@ import time
 
 from airflow.utils.decorators import apply_defaults
 
+from depc.extensions import redis_scheduler as redis
 from scheduler.dags.operators import QosOperator
 
 
@@ -10,9 +11,22 @@ class BeforeSubdagOperator(QosOperator):
     def __init__(self, params, *args, **kwargs):
         super(BeforeSubdagOperator, self).__init__(params, *args, **kwargs)
         self.count = params["count"]
+        self.excluded_nodes_from_average = params["excluded_nodes_from_average"]
 
     def execute(self, context):
         from depc.utils import get_start_end_ts
+
+        self.logger.info(
+            "Excluded nodes for {label}: {excluded}".format(
+                label=self.label, excluded=self.excluded_nodes_from_average
+            )
+        )
+        if self.excluded_nodes_from_average:
+            redis_key = "{team}.{label}.excluded_nodes_from_label_average".format(
+                team=self.team_name, label=self.label
+            )
+            redis.delete(redis_key)
+            redis.rpush(redis_key, *self.excluded_nodes_from_average)
 
         ds = context["ds"]
         start, end = get_start_end_ts(ds)
